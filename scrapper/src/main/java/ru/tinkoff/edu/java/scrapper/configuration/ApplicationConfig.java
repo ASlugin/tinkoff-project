3ba70e626dc9974ff95db997ffd83808133e61b2
ASlugin <slugin.alexander@gmail.com>
2023-03-28 23:39:43 +0300

    Add scheduling method to scrapper

--- scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/configuration/ApplicationConfig.java
+++ scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/configuration/ApplicationConfig.java
@@ -4,11 +4,14 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.validation.annotation.Validated;
+import ru.tinkoff.edu.java.scrapper.scheduler.Scheduler;
 
 @Validated
 @ConfigurationProperties(prefix = "app", ignoreUnknownFields = false)
 @OpenAPIDefinition(info = @Info(title = "Scrapper API",
         version = "1.0.1",
         description = "Some description of scrapper"))
-public record ApplicationConfig(@NotNull String test) {}
+@EnableScheduling
+public record ApplicationConfig(@NotNull String test, Scheduler scheduler) {}
--- scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/configuration/ClientConfiguration.java
+++ scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/configuration/ClientConfiguration.java
@@ -2,7 +2,6 @@ package ru.tinkoff.edu.java.scrapper.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.function.client.WebClient;
 import ru.tinkoff.edu.java.scrapper.client.GitHubClient;
 import ru.tinkoff.edu.java.scrapper.client.StackOverflowClient;
 
