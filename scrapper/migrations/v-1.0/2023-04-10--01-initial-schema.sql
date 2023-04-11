920f4ac1712b8af94c4d75582b327b5daa7af245
ASlugin <slugin.alexander@gmail.com>
2023-04-11 18:40:44 +0300

    Add creating tables and fix docker compose

--- scrapper/migrations/v-1.0/2023-04-10--01-initial-schema.sql
+++ scrapper/migrations/v-1.0/2023-04-10--01-initial-schema.sql
@@ -1,3 +1,14 @@
-CREATE TABLE chat (
-    chatID SERIAL PRIMARY KEY
+--liquibase formatted sql
+
+--changeset aslugin:create_chat_table
+CREATE table chat (
+    id SERIAL PRIMARY KEY
+);
+
+--changeset aslugin:create_link_table
+CREATE table link (
+    id SERIAL PRIMARY KEY,
+    url TEXT NOT NULL,
+    update_at TIMESTAMP with time zone DEFAULT now(),
+    chat_id INTEGER REFERENCES chat (id)
 );
