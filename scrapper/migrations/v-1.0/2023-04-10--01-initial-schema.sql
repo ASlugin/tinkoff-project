add3feeae8474c74db9d02cc86c27bbb777bb349
ASlugin <slugin.alexander@gmail.com>
2023-04-11 21:36:52 +0300

    Change relation to many-to-many

--- scrapper/migrations/v-1.0/2023-04-10--01-initial-schema.sql
+++ scrapper/migrations/v-1.0/2023-04-10--01-initial-schema.sql
@@ -9,6 +9,12 @@ CREATE table chat (
 CREATE table link (
     id SERIAL PRIMARY KEY,
     url TEXT NOT NULL,
-    update_at TIMESTAMP with time zone DEFAULT now(),
-    chat_id INTEGER REFERENCES chat (id)
+    update_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
 );
+
+--changeset aslugin:create_chat_link_table
+CREATE TABLE chat_link (
+    chat_id INTEGER REFERENCES chat(id),
+    link_id INTEGER REFERENCES link(id),
+    PRIMARY KEY (chat_id, link_id)
+)
