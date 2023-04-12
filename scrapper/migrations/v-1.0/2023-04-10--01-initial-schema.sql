5cdd40887aa6a258b481cbaef1308e88abe20c12
ASlugin <slugin.alexander@gmail.com>
2023-04-12 14:21:39 +0300

    Fix changesets and test

--- scrapper/migrations/v-1.0/2023-04-10--01-initial-schema.sql
+++ scrapper/migrations/v-1.0/2023-04-10--01-initial-schema.sql
@@ -1,19 +1,19 @@
 --liquibase formatted sql
 
 --changeset aslugin:create_chat_table
-CREATE table chat (
+CREATE TABLE IF NOT EXISTS chat (
     id SERIAL PRIMARY KEY
 );
 
 --changeset aslugin:create_link_table
-CREATE table link (
+CREATE TABLE IF NOT EXISTS link (
     id SERIAL PRIMARY KEY,
     url TEXT NOT NULL,
     update_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
 );
 
 --changeset aslugin:create_chat_link_table
-CREATE TABLE chat_link (
+CREATE TABLE IF NOT EXISTS chat_link (
     chat_id INTEGER REFERENCES chat(id),
     link_id INTEGER REFERENCES link(id),
     PRIMARY KEY (chat_id, link_id)
--- scrapper/src/main/test/ru/tinkoff/edu/java/scrapper/DataBaseTest.java
+++ scrapper/src/main/test/ru/tinkoff/edu/java/scrapper/DataBaseTest.java
@@ -1,36 +1,33 @@
 package ru.tinkoff.edu.java.scrapper;
 
+import lombok.SneakyThrows;
 import org.junit.Test;
 
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class DataBaseTest extends IntegrationEnvironment {
     @Test
+    @SneakyThrows
     public void tablesCreatedAfterMigrationsTest() {
-        try {
-            Connection connection = DriverManager.getConnection(
-                    DB_CONTAINER.getJdbcUrl(),
-                    DB_CONTAINER.getUsername(),
-                    DB_CONTAINER.getPassword()
-            );
+        Connection connection = DriverManager.getConnection(
+                DB_CONTAINER.getJdbcUrl(),
+                DB_CONTAINER.getUsername(),
+                DB_CONTAINER.getPassword()
+        );
 
-            List<String> tables = new ArrayList<>();
-            DatabaseMetaData metaData = connection.getMetaData();
-            ResultSet resultSet = metaData.getTables(null, null, "%", new String[]{"TABLE"});
-
-            while (resultSet.next()) {
-                tables.add(resultSet.getString("TABLE_NAME"));
-            }
-
-            assertTrue(tables.stream().anyMatch(x -> x.equals("chat")));
-            assertTrue(tables.stream().anyMatch(x -> x.equals("link")));
-            assertTrue(tables.stream().anyMatch(x -> x.equals("chat_link")));
+        List<String> tables = new ArrayList<>();
+        DatabaseMetaData metaData = connection.getMetaData();
+        ResultSet resultSet = metaData.getTables(null, null, "%", new String[]{"TABLE"});
+        while (resultSet.next()) {
+            tables.add(resultSet.getString("TABLE_NAME"));
         }
-        catch (SQLException exc) {
-            throw new RuntimeException(exc);
-        }
+
+        assertTrue(tables.stream().anyMatch(table -> table.equals("chat")));
+        assertTrue(tables.stream().anyMatch(table -> table.equals("link")));
+        assertTrue(tables.stream().anyMatch(table -> table.equals("chat_link")));
     }
 }
