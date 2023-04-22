74709b82997b5c1fc39e45109c3bf3ffb9d9eff2
ASlugin <slugin.alexander@gmail.com>
2023-04-22 17:51:59 +0300

    Implement jooq chat repository and service

--- scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/persistence/repository/jooq/JooqChatRepository.java
+++ scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/persistence/repository/jooq/JooqChatRepository.java
@@ -14,23 +14,33 @@ public class JooqChatRepository implements ChatRepository {
     @Override
     public Chat addChatById(long chatId) {
         dsl.insertInto(ru.tinkoff.edu.java.scrapper.domain.jooq.tables.Chat.CHAT,
-                ru.tinkoff.edu.java.scrapper.domain.jooq.tables.Chat.CHAT.ID).values(chatId).execute();
+                ru.tinkoff.edu.java.scrapper.domain.jooq.tables.Chat.CHAT.ID)
+                .values(chatId)
+                .execute();
         return findChatById(chatId);
     }
 
     @Override
     public void removeChatById(long chatId) {
-
+        dsl.deleteFrom(ru.tinkoff.edu.java.scrapper.domain.jooq.tables.Chat.CHAT)
+                .where(ru.tinkoff.edu.java.scrapper.domain.jooq.tables.Chat.CHAT.ID.eq(chatId))
+                .execute();
     }
 
     @Override
     public void removeChat(Chat chat) {
-
+        removeChatById(chat.getId());
     }
 
     @Override
     public Chat findChatById(long chatId) {
-        return null;
+        try {
+            return dsl.select().from(ru.tinkoff.edu.java.scrapper.domain.jooq.tables.Chat.CHAT)
+                    .where(ru.tinkoff.edu.java.scrapper.domain.jooq.tables.Chat.CHAT.ID.eq(chatId))
+                    .fetchOne().into(Chat.class);
+        } catch (NullPointerException exc) {
+            return null;
+        }
     }
 
     @Override
--- scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/service/jooq/JooqChatService.java
+++ scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/service/jooq/JooqChatService.java
@@ -2,9 +2,9 @@ package ru.tinkoff.edu.java.scrapper.service.jooq;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Primary;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
+import ru.tinkoff.edu.java.scrapper.persistence.model.Chat;
 import ru.tinkoff.edu.java.scrapper.persistence.repository.jooq.JooqChatRepository;
 import ru.tinkoff.edu.java.scrapper.service.TgChatService;
 
@@ -26,11 +26,12 @@ public class JooqChatService implements TgChatService {
 
     @Override
     public void unregister(long chatId) {
-
+        chatRepository.removeChatById(chatId);
     }
 
     @Override
     public boolean isChatExist(long chatId) {
-        return false;
+        Chat chat = chatRepository.findChatById(chatId);
+        return chat != null;
     }
 }
